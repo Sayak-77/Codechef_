74454866da3f73d5018e689e6bf8f5bfc25ea4d7
dragneel <sayakascent@gmail.com>
2021-11-14 14:55:39 +0530

    solution

--- optimalAdj.cpp
+++ optimalAdj.cpp
@@ -1,44 +1,73 @@
+/** PROBLEM CODE: REMCHR
+ * 
+ * Initially, Chef has a string S that contains only lowercase alphabets. In one operation Chef can choose two adjacent same characters 
+ * (if possible) and remove those two characters from the string S and its length will be reduced by two. Now Chef wants to perform this operation in a way such 
+ * that the final string length should be minimum (possibly empty). Help Chef to find the minimum possible length of the final string if Chef performs the operations optimally.
+
+Input:
+The first line of the input contains a single integer T denoting the number of test cases. The description of T test cases follows.
+The first and only line of each test case contains a single string S.
+Output
+For each test case, print a single line containing the answer.
+
+Constraints
+1≤T≤10
+1≤|S|≤105
+S contains only lowercase alphabets.
+Example Input
+4
+azyyza
+abacaba
+ppq
+pqrttyyr
+Example Output
+0
+7
+1
+2
+Explanation
+Example case 1: First remove 'y' and 'y', then 'z' and 'z' and finally 'a' and 'a'. The final string is an empty string.
+
+Example case 2: Chef can't remove any characters.
+
+Example case 3: The only adjacent pair that can be removed is 'p' and 'p'.
+
+Example case 4: One of the possible ways is first to remove 'y' and 'y', then 't' and 't' and finally 'r' and 'r'. The final string is "pq".
+**/
+
+
 #include <iostream>
 #include <string.h>
 #include <cstring>
+#include <stack>
 #include <algorithm>
 using namespace std;
 typedef long long int u;
-#define max 50;
-char* check(char *x,int n)
-{
-	int i,k=0;
-	for(i=1;i<n;i++)
-	{
-	    if(x[i]!=x[i-1])
-	    x[k++]=x[i-1];
-	    else
-	    ++i;
-	}
-	x[k++]=x[i-1];
-	x[k]='\0';
-	if(k!=n)
-	check(x,k);
-	else
-	return x;
-}
+#define len(a) a.size();
 int main() {
 	// your code goes here
-	int t,i;
-	string k;
-	char s[20];
-	char ch;
+	u t,i;
+	string s;
 	cin>>t;
 	while(t--)
 	{
+	    stack<char> k;
+	    char ch;
 	    cin>>s;
-	    for(i=0;i<strlen(s);i++)
+	    for(i=0;i<s.length();i++)
 	    {
-	        ch=tolower(s[i]);
-	        s[i]=ch;
+	        if(k.empty())
+	        k.push(s[i]);
+	        else
+	        {
+	            ch=k.top();
+	            if(ch==s[i])
+	            k.pop();
+	            else
+	            k.push(s[i]);
+	        }
 	    }
-	    k=check(s,strlen(s));
-	    cout<<k.length()<<endl;
+	    cout<<k.size()<<endl;
 	}
 	return 0;
 }
